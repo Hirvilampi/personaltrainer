fea8ecf145f87519eaef4f380a14c0a20254f699
Hirvilampi <bhr611@myy.haaga-helia.fi>
2025-05-08 19:22:43 +0300

    yritin taas monta tuntia saada tuota calenteria latautumaan aluksi, mutta sit se jää latautumaan jatkuasti. Parempi näin, että se on pakko ladata painamalla nappia

--- personaltrainer/src/Calendar.tsx
+++ personaltrainer/src/Calendar.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
 import dayGridPlugin from "@fullcalendar/daygrid";
+import weekGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import FullCalendar from "@fullcalendar/react";
-import { EventInput } from '@fullcalendar/core';
 
 // Register all Community features
 ModuleRegistry.registerModules([AllCommunityModule]);
@@ -50,20 +50,14 @@ type TTrainingsCustomerCustom = TTrainingsCustomer & {
     _links: TTrainingsData["_links"]
 }
 
-
 function Calendar() {
     const [trainingsWithLinks, setTrainingsWithLinks] = useState<TTrainingsCustomerCustom[]>([]);
-    const [events, setEvents] = useState<EventInput>([]);
-    const calendarRef = useRef(true); // viite kalenterikomponenttii
-    const [isFirstLoad, setIsFirstLoad] = useState(true); // Tila ensimmäiselle lataukselle
-    const [newCustomersFetch, setNewCustomerFetch] = useState(false);
-    const [treeniDataBase, setTreeniDataBase] = useState<TTrainingsCustomer[]>([]);
-    const [treeniDataBase2, setTreeniDataBase2] = useState<TTrainingsCustomer[]>([]);
+    const [events, setEvents] = useState<any>([]);
+    const [forceRenderKey, setForceRenderKey] = useState(0); // Tila uudelleenrenderöinnin pakottamiseen
 
     // haetaan treenajä ja asiakkkaita koskeva data ja yhdistetään ne
     const fetchCombinedTrainings = async () => {
         try {
-            console.log("LETS try to fetch")
             const [trainingsCustomersRes, trainingsLinksRes] = await Promise.all([
                 fetch(`${BASE_URL}/gettrainings`),
                 fetch(`${BASE_URL}/trainings`)
@@ -74,7 +68,6 @@ function Calendar() {
             }
 
             const treeniData: TTrainingsCustomer[] = await trainingsCustomersRes.json();
-
             const linkkiCustomeriinData: { _embedded: { trainings: TTrainingsData[] } } = await trainingsLinksRes.json();
             const linksMap = new Map<number, TTrainingsData["_links"]>();
 
@@ -93,34 +86,6 @@ function Calendar() {
                 }))
                 .filter(t => t._links); // suodata pois jos linkit puuttuvat
 
-            // Tarkista, onko data muuttunut
-   /*
-            if (treeniDataBase===null) {
-                setTreeniDataBase(treeniData);
-            } else {
-                setTreeniDataBase2(treeniDataBase);
-                setTreeniDataBase(treeniData);
-            }
-
-            const isTreeniDataChanged = JSON.stringify(treeniDataBase) !== JSON.stringify(treeniDataBase2);
-            console.log(treeniDataBase);
-            console.log(treeniDataBase2);
-            if (isTreeniDataChanged) {
-                setTrainingsWithLinks(combined);
-                setNewCustomerFetch(true); // Aseta flagi true, kun data muuttuu
-                console.log("treeniData päivitetty ja newCustomersFetch asetettu trueksi.");
-            }
-         
-            const isDataChanged = JSON.stringify(trainingsWithLinks) !== JSON.stringify(combined);
-            console.log("TRAININGS",JSON.stringify(trainingsWithLinks));
-            console.log("combined",JSON.stringify(combined));
-            if (isDataChanged) {
-                setTrainingsWithLinks(combined);
-                setNewCustomerFetch(true); // Aseta flagi true, kun data muuttuu
-                console.log("Data päivitetty ja newCustomersFetch asetettu trueksi.");
-            }
-                */
-
             setTrainingsWithLinks(combined);
 
         } catch (error) {
@@ -128,149 +93,62 @@ function Calendar() {
         }
     };
 
-    /*
-        useEffect(() => {
-            console.log("-- TrainingsWithLinks ladataan");
-            const events = trainingsWithLinks.map((training) => {
-                const startTime = new Date(training.date);
-                const endTime = new Date(startTime.getTime() + parseInt(training.duration) * 60000);
-    
-                return {
-                    title: `${training.activity} - ${training.customer.firstname} ${training.customer.lastname}`,
-                    start: startTime.toISOString(),
-                    end: endTime.toISOString(),
-                    extendedProps: {
-                        customer: training.customer,
-                        duration: training.duration,
-                    },
-                };
-            });
-            setEvents(events);
-        }, [trainingsWithLinks]);
-    */
-
-            // Ensimmäinen lataus
-    useEffect(() => {
-        console.log("Ensimmäinen lataus");
-        fetchCombinedTrainings(); // Kutsu vain kerran
-        
-    }, []);
-
     // Luo tapahtumat FullCalendar-komponentille
-    useEffect(() => {
-            console.log("-- TrainingsWithLinks ladataan");
-            const newEvents = trainingsWithLinks.map((training) => {
-                const startTime = new Date(training.date);
-                const endTime = new Date(startTime.getTime() + parseInt(training.duration) * 60000); // Kesto minuutteina
+    const calendarEvents = trainingsWithLinks.map((training) => {
+        const startTime = new Date(training.date);
+        const endTime = new Date(startTime.getTime() + parseInt(training.duration) * 60000); // Kesto minuutteina
 
-                return {
-                    title: `${training.activity} - ${training.customer.firstname} ${training.customer.lastname}`,
-                    start: startTime.toISOString(),
-                    end: endTime.toISOString(),
-                    extendedProps: {
-                        customer: training.customer,
-                        duration: training.duration,
-                    },
-                };
-            });
-            setEvents(newEvents);
-
-    }, []);
-
-    /*
-            // Luo tapahtumat FullCalendar-komponentille
-            const calendarEvents = trainingsWithLinks.map((training) => {
-                const startTime = new Date(training.date);
-                const endTime = new Date(startTime.getTime() + parseInt(training.duration) * 60000); // Kesto minuutteina
-        
-                return {
-                    title: `${training.activity} - ${training.customer.firstname} ${training.customer.lastname}`,
-                    start: startTime.toISOString(),
-                    end: endTime.toISOString(),
-                    extendedProps: {
-                        customer: training.customer,
-                        duration: training.duration,
-                    },
-                };
-            });
-    */
-
-/*
-    useEffect(() => {
-        console.log("-----****** SAA LATAUTUA VAIN KERRAN ****** ------- eka lataus, puoli sekuntia");
-       if (isFirstLoad){
-        const interval = setInterval(() => {
-            fetchCombinedTrainings();
-        }, 10000); // heti 0.1 sekunnin kuluttua, eikä sen jälkeen uudelleen 
-        setIsFirstLoad(false);
-        return () => clearInterval(interval);
-    }
-    }, []);
-
+        return {
+            title: `${training.activity} - ${training.customer.firstname} ${training.customer.lastname}`,
+            start: startTime.toISOString(),
+            end: endTime.toISOString(),
+            extendedProps: {
+                customer: training.customer,
+                duration: training.duration,
+            },
+        };
+    });
 
     useEffect(() => {
-        console.log("24h kuluttua");
-        const interval = setInterval(() => {
-            fetchCombinedTrainings();
-
-        }, 30 * 1000); // 24 tunnin välein
-        return () => clearInterval(interval);
-    }, [!isFirstLoad]);
-*/
-
-    useEffect(() => {
-        const fetchData = async () => { await fetchCombinedTrainings(); };
-          fetchData();
-    }, []);
-
-    /*
-        // Päivitä FullCalendar
-        useEffect(() => {
-            if (calendarRef.current) {
-                const calendarApi = calendarRef.current.getApi();
-                calendarApi.removeAllEvents();
-                events.forEach((event: EventInput) => calendarApi.addEvent(event));
-                calendarApi.updateSize();
-                console.log("Kalenteri päivitetty");
-            }
-        }, [events]);
-    */
+        fetchCombinedTrainings();
+        setEvents(calendarEvents);
+        setForceRenderKey((prevKey) => prevKey + 1);
+    }, []); // Lataa tiedot aina, kun sijainti muuttuu.
 
     const handleReload = () => {
         fetchCombinedTrainings(); // Lataa sivu uudelleen
-        console.log("HANDLERELOAD");
+        setEvents(calendarEvents);
+        setForceRenderKey((prevKey) => prevKey + 1);
     };
 
     return (
         <>
-            <div style={{ margin: "20px" }}>
-                <button onClick={handleReload} style={{ padding: "10px", margin: "10px", backgroundColor: "#4CAF50", color: "white", border: "none", borderRadius: "5px", cursor: "pointer" }}>
-                    Update calendar
-                </button>
-            </div>
+     <div style={{margin: "20px"}}>
+     <button onClick={handleReload} style={{ padding: "10px", margin: "10px", backgroundColor: "#4CAF50", color: "white", border: "none", borderRadius: "5px", cursor: "pointer" }}>
+            Update calendar
+        </button>
+     </div>
 
-            <div style={{ margin: "35px", width: "1100" }}>
-                <FullCalendar
-                    height="auto"
+       <div style={{margin:"35px"}}>
 
-                    plugins={[dayGridPlugin, timeGridPlugin]}
-                    headerToolbar={{
-                        left: 'prev,next today',
-                        center: 'title',
-                        right: 'dayGridMonth,timeGridWeek,timeGridDay',
-                    }}
-                    initialView="timeGridWeek"
-                    events={events}
-                    firstDay={1}
-                    scrollTime="06:00:00"
-                    slotMinTime="07:00:00"
-                    locale="fi"
-                //              ref={calendarRef}
-                //               key={forceRenderKey}
-                />
+            <FullCalendar
+                key={forceRenderKey}
+                plugins={[dayGridPlugin, weekGridPlugin, timeGridPlugin]}
+                headerToolbar={{
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay',
+                  }}
+                initialView="timeGridWeek"
+                events={events}
+                firstDay={1}
+                slotMinTime="06:00:00"
+                scrollTime="07:00:00"
+                locale="fi"
+            />
 
 
-            </div>
+         </div>
         </>
 
     )
